c5801c66fdd6358cd22ecf0d44ae6f8006f35aea
DESKTOP-0U18BFB\Pascu <David P>
2023-08-19 18:54:58 +0200

    Creacion del 3 syso

--- pruebaGit/src/App.java
+++ pruebaGit/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Cambio 2 desde Github");
         System.out.println("Cambio 3 desde visual");
+        System.out.println("Cambio 4 desde visual con la clase creada");
     }
 }
