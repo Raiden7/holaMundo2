6fa8f7e5ef9df1853241f8a18b581c2ca8b61fe5
DESKTOP-0U18BFB\Pascu <David P>
2023-08-19 18:53:21 +0200

    Cambio 3 desde visual arreglado error

--- pruebaGit/src/App.java
+++ pruebaGit/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Cambio 2 desde Github");
+        System.out.println("Cambio 3 desde visual");
     }
 }
