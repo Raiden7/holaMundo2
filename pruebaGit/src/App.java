0ff0602a408d65aedc95359ec4c2490de64be16b
Raiden7 <pascu1987@hotmail.com>
2023-08-19 19:00:24 +0200

    Cambio brach main

--- pruebaGit/src/App.java
+++ pruebaGit/src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Cambio 2 desde Github");
         System.out.println("Cambio 3 desde visual");
         System.out.println("Cambio 4 desde visual con la clase creada");
+         System.out.println("Sigo trabajando enel brach principalb");
     }
 }
